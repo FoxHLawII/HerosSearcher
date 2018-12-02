d11ceef803560a7f5474d48a6cac8cca12e1eee4
RumplesT <jfrojas251@gmail.com>
2018-12-02 10:14:38 -0500

    Add router and config, routerLink and routerLinksActive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { AboutComponent } from "./components/about/about.component";
+import { HerosComponent } from "./components/heros/heros.component";
 
 const APP_ROUTES: Routes = [
   {
     path: "home",
     component: HomeComponent
+  },
+  {
+    path: "heros",
+    component: HerosComponent
   },
   {
     path: "about",
